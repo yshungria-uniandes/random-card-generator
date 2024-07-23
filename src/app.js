8ff563a7bb8b91a8e3fd97e68a440dc3cbdf10f2
yshungria-uniandes <ys.hungria@uniandes.edu.co>
2024-07-23 17:11:41 -0500

    Random Card Generator created

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  function randomEvent(list) {
+    let numRandom = Math.floor(Math.random() * list.length);
+    return list[numRandom];
+  }
+
+  let symbols = ["♦", "♥", "♠", "♣"];
+  let numbers = [
+    "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"
+  ];
+
+  let cardSymbolTop = randomEvent(symbols);
+  let elementCardSymbolTop = document.querySelector("#cardSymbolTop");
+  elementCardSymbolTop.innerHTML = cardSymbolTop;
+
+  let numCard = randomEvent(numbers);
+  let elementNumCard = document.querySelector("#cardNumber");
+  elementNumCard.innerHTML = numCard;
+
+  let elementCardSymbolBottom = document.querySelector("#cardSymbolBottom");
+  elementCardSymbolBottom.innerHTML = cardSymbolTop;
+  
+  elementCardSymbolBottom.style.transform = "rotate(180deg)";
+
+  if (cardSymbolTop === "♥" || cardSymbolTop === "♦") {
+    let elements = document.querySelectorAll(".cardSymbol");
+    elements.forEach(function (element) {
+      element.style.color = "red";
+    });
+  }
 };
